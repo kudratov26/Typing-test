81f4f5cc11a63a25eea4978f99eac550935bb694
lazeez <qudratov.com26@gmail.com>
2025-01-05 11:38:12 +0500

    done!

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // List Words
-const wordList = 'Hello! I think you did a great job with this video! I have a question though, I was looking through the comment section and found the comment noticing the bug and the solution to delete a previous child when it is incorrect, it worked for me flawlessly. Although, I am running into a similar issue when I hit the space bar after typing in a incorrect key that it does not erase the previous value that you input on the previous word. For example: if the word is there and I type theree and backspace to try to delete it, it will change the word to theree as the correct value when you try to go to the next word. Not sure if that made sense. I hope it did. Any solution to this problem? I have been trying to debug this for a couple days now (I am a beginner) and would like some help. Any help is appreciated. Thank you again for your hard work!'.split(" ")
-
+const wordList = 'in one good real one not school set they state high life consider on and not come what also for set point can want as while with of order child about school thing never hold find order each too between program work end you home place around while place problem end begin interest while public or where see time those increase interest be give end think seem small as both another a child same eye you between way do who into again good fact than under very head become real possible some write know however late each that with because that place nation only for each change form consider we would interest with world so order or run more open that large write turn never over open each over change still old take hold need give by consider line only leave while what set up number part form want against great problem can because head so first this here would course become help year first end want both fact public long word down also long for without new turn against the because write seem line interest call not if line thing what work people way may old consider leave hold want life between most place may if go who need fact such program where which end off child down change to from people high during people find to however into small new general it do that could old for last get another hand much eye great no work and with but good there last think can around use like number never since world need what we around part show new come seem while some and since still small these you general which seem will place come order form how about just also they with state late use both early too lead general seem there point take general seem few out like might under if ask while such interest feel word right again how about system such between late want fact up problem stand new say move a lead small however large public out by eye here over so be way use like say people work for since interest so face order school good not most run problem group run she late other problem real form what just high no man do under would to each too end point give number child through so this large see get form also all those course to work during about he plan still so like down he look down where course at who plan way so since come against he all who at world because while so few last these mean take house who old way large no first too now off would in this course present order home public school back own little about he develop of do over help day house stand present another by few come that down last or use say take would each even govern play around back under some line think she even when from do real problem between long as there school do as mean to all on other good may from might call world thing life turn of he look last problem after get show want need thing old other during be again develop come from consider the now number say life interest to system only group world same state school one problem between for turn run at very against eye must go both still all a as so after play eye little be those should out after which these both much house become both school this he real and may mean time by real number other as feel at end ask plan come turn by all head increase he present increase use stand after see order lead than system here ask in of look point little too without each for both but right we come world much own set we right off long those stand go both but under now must real general then before with much those at no of we only back these person plan from run new as own take early just increase only look open follow get that on system the mean plan man over it possible if most late line would first without real hand say turn point small set at in system however to be home show new again come under because about show face child know person large program how over could thing from out world while nation stand part run have look what many system order some one program you great could write day do he any also where child late face eye run still again on by as call high the must by late little mean never another seem to leave because for day against public long number word about after much need open change also'.split(' ');
 
 // Elements
-const words = document.getElementById("words")
-const game = document.getElementById("game")
-const cursor = document.getElementById("cursor")
+const words = document.getElementById("words");
+const game = document.getElementById("game");
+const cursor = document.getElementById("cursor");
+const info = document.getElementById("info");
 
-const wordLeng = wordList.length
+const wordLeng = wordList.length;
+const gameTime = 9*1000;
+
+window.timer = null;
+window.gameStart = null;
 
 // Functions
 
@@ -26,14 +30,41 @@ function randomWord(){
 function formatWord(word) {
   return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
 }
+
 function newGame(){
+    game.classList.add("start")
+    if(game.classList.contains('over')) {
+        game.classList.replace('over', 'start');
+    }
+
     words.innerHTML = "";
     for(let i = 0; i < 200; i++){
         words.innerHTML += formatWord(randomWord());
     }
     addClass(document.querySelector('.word'), 'current');
     addClass(document.querySelector('.letter'), 'current');
+    window.timer = null;
 }
+
+function getWpm() {
+    const words = [...document.querySelectorAll(".word")];
+    const lastTypedWord = document.querySelector(".word.current");
+    const lastTypedWordIndex = words.indexOf(lastTypedWord);
+    const typedWords = words.slice(0, lastTypedWordIndex);
+    const correctWords = typedWords.filter(word => {
+        const letters = [...word.children];
+        const incorrectLetter = letters.filter(letter => letter.classList.contains("incorrect"));
+        const correctLetter = letters.filter(letter => letter.classList.contains("correct"));
+        return incorrectLetter.length === 0 && correctLetter.length === letters.length;
+    })
+    return Math.round(correctWords.length / gameTime * 60000);
+}
+function gameOver() {
+    clearInterval(window.timer);
+    game.classList.replace('start','over');
+    const result = getWpm();
+    document.getElementById('info').innerHTML = `WPM: ${result}`;
+}  
 
 game.addEventListener("keydown", ev => {
     const key = ev.key;
@@ -46,7 +77,28 @@ game.addEventListener("keydown", ev => {
     const isFirstLetter = currentLetter === currentWord.firstChild;
     const isExtra = document.querySelector(".letter.incorrect.extra");
 
+    if(document.querySelector('#game.over')) return
+    
     console.log({key, expected});
+
+    if (!window.timer && isLetter) {
+        document.getElementById('info').innerHTML = gameTime/1000 + 1;
+        window.timer = setInterval(() => {
+          if (!window.gameStart) {
+            window.gameStart = (new Date()).getTime();
+          }
+          const currentTime = (new Date()).getTime();
+          const msPassed = currentTime - window.gameStart;
+          const sPassed = Math.round(msPassed / 1000);
+          const sLeft = Math.round((gameTime / 1000) - sPassed);
+          if (sLeft <= 0) {
+            gameOver();
+            return;
+          }
+          document.getElementById('info').innerHTML = sLeft + '';
+        }, 1000);
+      }
+    
 
     if(isLetter){
         if(currentLetter){
@@ -109,14 +161,6 @@ game.addEventListener("keydown", ev => {
         
     }
 
-    // move lines / words (scrolling next)
-    // if (currentWord.getBoundingClientRect().top > 280) {
-    //     const words = document.getElementById('words');
-    //     const margin = parseInt(words.style.marginTop || '0px');
-    //     words.style.marginTop = `${margin - 35}px`;
-    //     // words.style.marginTop = 
-    // }
-
     // make the content / words or lines to auto scroll whenever the user types and gets to the bottom
     const words = document.getElementById('words');
     const margin = parseInt(words.style.marginTop || '0px');
@@ -138,6 +182,11 @@ game.addEventListener("keydown", ev => {
     cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? "left" : "right"] + "px";
 
 })
+
+document.getElementById('newGameBtn').addEventListener('click', () => {
+    gameOver();
+    newGame();
+});
 
 newGame();
 
